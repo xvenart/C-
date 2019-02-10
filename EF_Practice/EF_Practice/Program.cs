a55f0799692a2c1351a832cb3b8a489da10702b3
Unknown <tvdlover19@gmail.com>
2019-02-11 01:25:25 +0400

    Task 5.3

--- EF_Practice/EF_Practice/Program.cs
+++ EF_Practice/EF_Practice/Program.cs
@@ -24,7 +24,7 @@ namespace EF_Practice
     {
         static double SetLength(Point obj)
         {
-            return Math.Sqrt(Math.Pow(Math.Abs(obj.X - 500), 2) + Math.Pow(Math.Abs(obj.Y - 500), 2));
+            return Math.Sqrt(Math.Pow(obj.X - 500, 2) + Math.Pow(obj.Y - 500, 2));
         }
 
         static int SetGroup(int xx, int yy)
@@ -34,65 +34,87 @@ namespace EF_Practice
 
         static Category SetCategory(int xx, int yy)
         {
-            return ((xx >= 0 && xx < 500 && yy >= 0 && yy < 500) ? Category.ThirdQ : (xx > 500 && xx <= 1000 && yy >= 0 && yy < 500) ? Category.FourthQ : (xx > 500 && xx <= 1000 && yy > 500 && yy <= 1000) ? Category.FirstQ : (xx >= 0 && xx < 500 && yy > 500 && yy <= 1000) ? Category.SecondQ : Category.ZeroP);
+            if (xx < 0 || xx > 1000 || yy < 0 || yy > 1000)
+            {
+                throw new ArgumentOutOfRangeException("X и/или Y выходит за границы интервалов.");
+            }
+
+            return ((xx < 500 &&  yy < 500) ? Category.ThirdQ :
+                (xx > 500 && yy < 500) ? Category.FourthQ :
+                (xx > 500 && yy > 500) ? Category.FirstQ :
+                (xx < 500 && yy > 500) ? Category.SecondQ : Category.ZeroP);
         }
 
         static void Main(string[] args)
         {
-            var rand = new Random();
-
-            Console.WriteLine("Случайные точки:");
-
-            var p = new List<Point>();
-            for (int i = 0; i < 5; i++)
+            try
             {
-                var xx = rand.Next(0, 1000);
-                var yy = rand.Next(0, 1000);
-                p.Add(new Point { Id = i + 1, X = xx, Y = yy, Group = SetGroup(xx, yy), Category = SetCategory(xx, yy) });
-                Console.WriteLine($"x = {p[i].X}, y = {p[i].Y}, group = {p[i].Group}, category = {p[i].Category}");
-            }
-            Console.WriteLine();
+                var rand = new Random();
 
-            var zp = new Point { Id = 999, X = 500, Y = 500, Group = SetGroup(500, 500), Category = SetCategory(500, 500) };
+                Console.WriteLine("Случайные точки:");
 
-            Console.WriteLine("Нулевая точка:");
-            Console.WriteLine($"x = {zp.X}, y = {zp.Y}, group = {zp.Group}, category = {zp.Category}");
-            Console.WriteLine();
-
-            Console.WriteLine("Расстояние до нулевой точки:");
-            foreach (var i in p)
-            {
-                i.Length = SetLength(i);
-                Console.WriteLine($"x: {i.X}, y: {i.Y} => {i.Length}");
-            }
-            Console.WriteLine();
-
-            Console.WriteLine("Сортировка:");
-            var result = p.OrderBy(x => x.Length);
-            foreach (var i in result)
-            {
-                Console.WriteLine($"x: {i.X}, y: {i.Y} => {i.Length}");
-            }
-
-            using (PointContext db = new PointContext())
-            {
-                db.Points.RemoveRange(db.Points);
-                db.SaveChanges();
-
-                foreach (Point i in p)
+                var p = new List<Point>();
+                for (int i = 0; i < 5; i++)
                 {
-                    db.Points.Add(i);
+                    var xx = rand.Next(0, 1000);
+                    var yy = rand.Next(0, 1000);
+                    p.Add(new Point { Id = i + 1, X = xx, Y = yy, Group = SetGroup(xx, yy), Category = SetCategory(xx, yy) });
+                    Console.WriteLine($"x = {p[i].X}, y = {p[i].Y}, group = {p[i].Group}, category = {p[i].Category}");
                 }
-                
-                db.SaveChanges();
+                Console.WriteLine();
 
-                Console.WriteLine("Объект сохранён.");
+                /*//Проверка на выходы за границы
+                var CheckX = 1001;
+                var CheckY = -9;
+                p.Add(new Point { Id = 9999, X = CheckX, Y = CheckY, Group = SetGroup(CheckX, CheckY), Category = SetCategory(CheckX, CheckY) });*/
 
-                Console.WriteLine("Список объектов:");
-                foreach(Point i in db.Points)
+                var zp = new Point { Id = 0, X = 500, Y = 500, Group = SetGroup(500, 500), Category = SetCategory(500, 500) };
+
+                Console.WriteLine("Нулевая точка:");
+                Console.WriteLine($"x = {zp.X}, y = {zp.Y}, group = {zp.Group}, category = {zp.Category}");
+                Console.WriteLine();
+
+                Console.WriteLine("Расстояние до нулевой точки:");
+                foreach (var i in p)
                 {
-                    Console.WriteLine($"{i.Id} | {i.X}, {i.Y} => {i.Length} | {i.Group} | {i.Category}");
+                    i.Length = SetLength(i);
+                    Console.WriteLine($"x: {i.X}, y: {i.Y} => {i.Length}");
                 }
+                Console.WriteLine();
+
+                Console.WriteLine("Сортировка:");
+                var result = p.OrderBy(x => x.Length);
+                foreach (var i in result)
+                {
+                    Console.WriteLine($"x: {i.X}, y: {i.Y} => {i.Length}");
+                }
+
+                using (PointContext db = new PointContext())
+                {
+                    if (db.Points.Count() != 0)
+                    {
+                        db.Points.RemoveRange(db.Points);
+                        db.SaveChanges();
+                    }
+
+                    foreach (var i in p)
+                    {
+                        db.Points.Add(i);
+                    }
+
+                    db.SaveChanges();
+                    Console.WriteLine("Объект сохранён.");
+
+                    Console.WriteLine("Список объектов:");
+                    foreach (var i in db.Points)
+                    {
+                        Console.WriteLine($"{i.Id} | {i.X}, {i.Y} => {i.Length} | {i.Group} | {i.Category}");
+                    }
+                }
+            }
+            catch (ArgumentOutOfRangeException e)
+            {
+                Console.WriteLine($"Ошибка: {e.Message}");
             }
             Console.Read();
         }
